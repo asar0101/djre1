62d4651f1fc2a78b3e80ad52b0c3573cbb6bfb49
asar0101 <140738676+asar0101@users.noreply.github.com>
2023-10-11 12:07:28 +0200

    Add files via upload

---
+++ frontend/src/store.js
@@ -0,0 +1,116 @@
+import { legacy_createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+    productListReducer,
+    productDetailsReducer,
+    productDeleteReducer,
+    productCreateReducer,
+    productUpdateReducer,
+    productReviewCreateReducer,
+    productTopRatedReducer,
+    
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer,
+    userListReducer,
+    userDeleteReducer,
+    userUpdateReducer,
+} from './reducers/userReducers'
+import {
+    orderCreateReducer,
+    orderDetailsReducer,
+    orderPayReducer,
+    orderListMyReducer,
+    orderListReducer,
+    orderDeliverReducer,
+} from './reducers/orderReducers'
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
+    productTopRated: productTopRatedReducer,
+
+
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
+    orderList: orderListReducer,
+    orderDeliver: orderDeliverReducer,
+    
+    
+})
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null  
+    
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+    JSON.parse(localStorage.getItem('shippingAddress')) : {}   
+
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod') ?
+    JSON.parse(localStorage.getItem('paymentMethod')) : {}      
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage,
+        paymentMethod: paymentMethodFromStorage,
+    },
+    userLogin: { userInfo: userInfoFromStorage },
+}
+
+
+const middleware = [thunk]
+
+const store = legacy_createStore(reducer, initialState,
+    composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @deprecated
+// We recommend using the configureStore method of the @reduxjs/toolkit package, which replaces createStore.
+// Redux Toolkit is our recommended approach for writing Redux logic today, including store setup, reducers, data fetching, and more.
+// For more details, please read this Redux docs page: https://redux.js.org/introduction/why-rtk-is-redux-today
+// configureStore from Redux Toolkit is an improved version of createStore that simplifies setup and helps avoid common bugs.
+// You should not be using the redux core package by itself today, except for learning purposes. The createStore method from the core redux package will not be removed, but we encourage all users to migrate to using Redux Toolkit for all Redux code.
+// If you want to use createStore without this visual deprecation warning, use the legacy_createStore import instead:
+// import { legacy_createStore as createStore} from 'redux'
